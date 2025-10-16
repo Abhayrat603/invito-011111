188388e6a8faf59b52c85a65c3b140e2c6df023b
Abhay Rathour <abhayrat603@gmail.com>
2025-10-16 06:10:43 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.imghippo.com',
+        port: '',
+        pathname: '/**',
+      },
     ]
   }
 }
