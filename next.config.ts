8b6238a0a59a95d25faaabef822a825840702245
Abhay Rathour <abhayrat603@gmail.com>
2025-10-16 06:02:11 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'jumpshare.com',
+        port: '',
+        pathname: '/**',
+      },
     ]
   }
 }
