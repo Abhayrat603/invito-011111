6e26db0ac9deda23163da9bea7d066e9996befa4
Abhay Rathour <abhayrat603@gmail.com>
2025-10-16 05:57:34 +0000

    kisi ka use mat kro iss firebase me sab save kro sab kuch isis firbase d

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,18 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = { 
-  apiKey: "AIzaSyAooFvH9d19W2jcNOAmZv7GabHCdr_O3RE", 
-  authDomain: "abhay-67783.firebaseapp.com", 
-  databaseURL: "https://abhay-67783-default-rtdb.firebaseio.com", 
-  projectId: "abhay-67783", 
-  storageBucket: "abhay-67783.firebasestorage.app", 
-  messagingSenderId: "880880220147", 
-  appId: "1:880880220147:web:fe02b389e8d28463a28486", 
-  measurementId: "G-2R4RJ2976X" 
+const firebaseConfig = {
+  apiKey: "AIzaSyAooFvH9d19W2jcNOAmZv7GabHCdr_O3RE",
+  authDomain: "abhay-67783.firebaseapp.com",
+  databaseURL: "https://abhay-67783-default-rtdb.firebaseio.com",
+  projectId: "abhay-67783",
+  storageBucket: "abhay-67783.firebasestorage.app",
+  messagingSenderId: "880880220147",
+  appId: "1:880880220147:web:fe02b389e8d28463a28486",
+  measurementId: "G-2R4RJ2976X"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
